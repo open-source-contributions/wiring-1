2c10897cf8cf4d0a86e1ab7d5e9e2837b7cf83a0
Anderson Costa <arcostasi@gmail.com>
2016-03-09 00:01:29 -0300

    Abstract controller updated

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,6 +2,7 @@
 
 namespace Wiring\Controller;
 
+use Exception;
 use Interop\Container\ContainerInterface;
 
 abstract class AbstractController implements ControllerInterface
@@ -41,5 +42,64 @@ abstract class AbstractController implements ControllerInterface
     public function has($id)
     {
         return $this->container->has($id);
+    }
+
+    /**
+     * Resolves an entry by its name.
+     * If given a class name, it will return a new instance of that class.
+     *
+     * @param string $name Entry name or a class name.
+     * @param array $parameters Optional parameters to use to build the entry. Use this to force specific
+     *                           parameters to specific values. Parameters not defined in this array will
+     *                           be automatically resolved.
+     *
+     * @throws \Exception       Error while resolving the entry.
+     * @return mixed
+     */
+    public function make($name, array $parameters = [])
+    {
+        if (!method_exists($this->container, 'make')) {
+            throw new Exception('Container method not found');
+        }
+
+        return $this->container->make($name, $parameters);
+    }
+
+    /**
+     * Call the given function using the given parameters.
+     *
+     * Missing parameters will be resolved from the container.
+     *
+     * @param callable $callable Function to call.
+     * @param array $parameters Parameters to use. Can be indexed by the parameter names
+     *                             or not indexed (same order as the parameters).
+     *                             The array can also contain DI definitions, e.g. DI\get().
+     *
+     * @throws \Exception
+     * @return mixed Result of the function.
+     */
+    public function call($callable, array $parameters = [])
+    {
+        if (!method_exists($this->container, 'call')) {
+            throw new Exception('Container method not found');
+        }
+
+        return $this->container->call($callable, $parameters);
+    }
+
+    /**
+     * Define an object or a value in the container.
+     *
+     * @param string $name Entry name
+     * @param mixed $value Value, use definition helpers to define objects
+     * @throws \Exception
+     */
+    public function set($name, $value)
+    {
+        if (!method_exists($this->container, 'set')) {
+            throw new Exception('Container method not found');
+        }
+
+        return $this->container->set($name, $value);
     }
 }
