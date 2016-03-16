da35f0ebc9a1599ba763b4609eee8d4139c64e3c
Anderson Costa <arcostasi@gmail.com>
2016-03-15 23:16:30 -0300

    Added restful factory routes

--- src/Factory/FastRouteFactory.php
+++ src/Factory/FastRouteFactory.php
@@ -4,6 +4,8 @@ namespace Wiring\Factory;
 
 use FastRoute;
 use FastRoute\RouteCollector;
+use ReflectionClass;
+use Wiring\Controller\RestfulControllerInterface;
 
 class FastRouteFactory
 {
@@ -26,13 +28,48 @@ class FastRouteFactory
      */
     public function getDispatcher()
     {
-        $dispatcher = FastRoute\simpleDispatcher(function(RouteCollector $route) {
+        $dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $route) {
             // Added all routes
             foreach ($this->routes as $param) {
-                $route->addRoute($param[0], $param[1], $param[2]);
+                // Check method is defined
+                if (count($param[2]) == 2) {
+                    $route->addRoute($param[0], $param[1], $param[2]);
+                } elseif (isset($param[2][0])) {
+                    // Create object reflection
+                    $obj = new ReflectionClass($param[2][0]);
+                    // Check object implements restful interface
+                    if ($obj->implementsInterface(RestfulControllerInterface::class)) {
+                        // Get restful methods
+                        $this->addRestfulMethods($route, $param);
+                    }
+                }
             }
         });
 
         return $dispatcher;
+    }
+
+    /**
+     * Added restful routes.
+     *
+     * @param RouteCollector $route
+     * @param array $param
+     */
+    private function addRestfulMethods($route, $param)
+    {
+        $uri = $param[1];
+        $class = $param[2][0];
+
+        $methods = [
+            'index' => ['GET', $uri],
+            'read' => ['GET', $uri . '/{id}'],
+            'create' => ['POST', $uri],
+            'update' => ['PUT', $uri . '/{id}'],
+            'delete' => ['DELETE', $uri . '/{id}'],
+        ];
+
+        foreach ($methods as $key => $method) {
+            $route->addRoute($method[0], $method[1], [$class, $key]);
+        }
     }
 }
